2ef5d87d8a7d248685a10d1b064906dae4bbe65d
Minh Tiến <minhati0777@gmail.com>
2022-07-21 18:57:26 +0700

    Display

--- DataAccess/Models/FStoreDBContext.cs
+++ DataAccess/Models/FStoreDBContext.cs
@@ -28,7 +28,7 @@ namespace DataAccess.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.;Uid=sa;Pwd=cuong0888358309;Database=FStoreDB");
+                optionsBuilder.UseSqlServer("Server=.;Uid=sa;Pwd=123;Database=FStoreDB");
             }
         }
 
